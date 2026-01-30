ee99301beb2fce52f607499095f54490c4208108
robertpanvip <robertpanvip@163.com>
2026-01-30 20:59:09 +0800

    feat:init

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,6 +48,8 @@ tasks {
         targetCompatibility = "21"
     }
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "21"
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
+        }
     }
 }
